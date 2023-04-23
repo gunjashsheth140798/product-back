ac7f0816efca3125e7ce34fcfcedb893cfa78e07
gunjashsheth140798 <gunjashsheth140798@gmail.com>
2023-04-23 20:03:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,24 +30,26 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: "https://dreamy-dieffenbachia-6d5e2e.netlify.app/",
+  origin: "*",
+  methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
 };
 
 app.use(cors(corsOptions));
 
 // CORS
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  next();
-});
+
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   res.header(
+//     "Access-Control-Allow-Methods",
+//     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+//   );
+//   next();
+// });
 
 app.use(express.static(__dirname + "/public"));
 
